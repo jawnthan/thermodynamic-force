dd03d029e3c8994474cb99bc98a15fd100825db9
John Whittaker <johnwhittake@zatar.imp.fu-berlin.de>
2019-12-12 17:15:27 +0100

    smooth_dens now builds the tf table

--- smooth_dens.sh
+++ smooth_dens.sh
@@ -20,10 +20,9 @@ stop  = tgt[-1,0]
 T   = 298
 kbT = -1.0*0.00831451*T
 prefac = 0.001
+rbox   = 5.8
 
-rnew = linspace(start, stop, num=100)
-
-step = (rnew[-1] - rnew[-2])
+rnew = linspace(start, stop, num=500)
 
 tck        = interpolate.splrep(r, dens, s=0)
 smoothDens = interpolate.splev(rnew, tck, der=0)
@@ -32,20 +31,29 @@ derivative = interpolate.splev(rnew, tck, der=1)
 pot     = smoothDens*kbT*prefac
 tf      = derivative*prefac
 
+step = (rnew[-1] - rnew[-2])
 
+first   = arange(0.0, start, step)
+second  = arange(stop+step, rbox, step)
 
+first0  = zeros(len(first))
+second0 = zeros(len(second))
 
+first_pot  = full(len(first), pot[0])
+second_pot = full(len(second), pot[-1])
 
+x     = concatenate((first, rnew, second))
+y_tf  = concatenate((first0, tf, second0))
+y_pot = concatenate((first_pot, pot, second_pot))
 
+SPL = column_stack((rnew, smoothDens))
+savetxt('smoothDens', SPL)
 
-#SPL = column_stack((rnew, smoothDens))
-#savetxt('smoothDens', SPL)
-#
-#DAT = column_stack((rnew, tf))
-#savetxt('derivative', DAT)
-#
-#POT = column_stack((rnew, pot))
-#savetxt('potential', POT)
+DAT = column_stack((x, y_tf))
+savetxt('derivative', DAT)
+
+POT = column_stack((x, y_pot))
+savetxt('potential', POT)
 
 #plt.plot(r, dens) 
 #plt.plot(rnew, smoothDens)
